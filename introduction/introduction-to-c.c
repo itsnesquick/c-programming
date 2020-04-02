add53d3dd511197f21e045209fbe6fdd8b7eed3f
Nesquick <nesquick@Nesquicks-MBP.fritz.box>
2020-04-02 16:40:29 +0200

    Added introduction directorey

---
+++ introduction/introduction-to-c.c
@@ -0,0 +1,8 @@
+/* First example of a c program */
+#include <stdio.h>
+main() {
+    printf("\nThis is the first, very simple c program.\n");
+    printf("\n'printf' cannot only print text\n");
+    printf("but also calculate certain things for us.\n\n");
+    printf("8 * 23 = %i \n", 8 * 23);
+}
